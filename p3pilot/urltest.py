6dd9fdc9f4831615e757f9458c34a92a36760ec0
Maxim Potekhin <potekhin@bnl.gov>
2016-10-27 16:23:37 -0400

    Added useful args

--- p3pilot/urltest.py
+++ p3pilot/urltest.py
@@ -4,27 +4,45 @@ import argparse
 
 import urllib
 from urllib import request
+from urllib import error
+from urllib.error import URLError
 
 #-------------------------
 parser = argparse.ArgumentParser()
+
 parser.add_argument("-u", "--url", type=str, default='http://localhost:8000/')
+parser.add_argument("-v", "--verbosity", type=int, default=0, choices=[0, 1, 2],
+                    help="increase output verbosity")
+
 args = parser.parse_args()
 
-url = args.url
-print(url)
+url	= args.url
+verb	= args.verbosity
 
-response = urllib.request.urlopen(url)
-print ('RESPONSE:', response)
-print ('URL     :', response.geturl())
-
+try:
+    response = urllib.request.urlopen(url)
+except URLError:
+    exit(1)
+    
+    
 headers = response.info()
-print ('DATE    :', headers['date'])
-print ('HEADERS :')
-print ('---------')
-print (headers)
-
 data = response.read()
-print ('LENGTH  :', len(data))
-print ('DATA    :')
-print ('---------')
-print (data)
+
+if(verb >=2):
+    print ('RESPONSE:', response)
+    print ('URL     :', response.geturl())
+
+    print ('DATE    :', headers['date'])
+    print ('HEADERS :')
+    print ('---------')
+    print (headers)
+
+    print ('LENGTH  :', len(data))
+    print ('DATA    :')
+    print ('---------')
+    
+if(verb >0):
+    print (data)
+    
+exit(0)
+
