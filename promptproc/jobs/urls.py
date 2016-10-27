85e35f923aa251da979a3acbbc67c38a94ea170e
Maxim Potekhin <potekhin@bnl.gov>
2016-10-27 18:10:56 -0400

    More tweaking...

--- promptproc/jobs/urls.py
+++ promptproc/jobs/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<job_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^$', views.detail, name='detail'),
+#    url(r'^(?P<job_id>[0-9]+)/$', views.detail, name='detail'),
+#    url(r'^$', views.index, name='index'),
 ]
