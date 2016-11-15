3b8f911646e361f78cd583e13fba29b01259020e
Maxim Potekhin <potekhin@bnl.gov>
2016-11-15 13:22:20 -0500

    Stubbing out the monitor

--- promptproc/monitor/views.py
+++ promptproc/monitor/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
 
-# Create your views here.
+import datetime
+import uuid
+
+from django.shortcuts			import render
+from django.http			import HttpResponse
+from django.views.decorators.csrf	import csrf_exempt
+from django.utils			import timezone
+from django.core			import serializers
+
+from .models import pilot
+from jobs.models import job
+
+#########################################################
+# TZ-awarewness:					#
+# The following is not TZ-aware: datetime.datetime.now()#
+# so we are using timzone.now() where needed		#
+#########################################################
+
+def detail(request):
+    pilot_uuid	= request.GET.get('uuid','')
+    pilot_pk	= request.GET.get('pk','')
+
+    if(pilot_uuid == '' and pilot_pk == ''):
+        return HttpResponse("Empty pilot id")
+
+    if(pilot_uuid != ''):
+        p = pilot.objects.get(uuid=pilot_uuid)
+    else:
+        p = pilot.objects.get(pk=pilot_pk)
+        
+    data = serializers.serialize('json', [ p, ])
+    
+    return HttpResponse(data)
+
+
