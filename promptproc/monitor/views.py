85d798c9437baf036e914953d6f30023412fea8c
Maxim Potekhin <potekhin@bnl.gov>
2016-11-17 11:36:01 -0500

    retooling for much shorter code in the monitor view

--- promptproc/monitor/views.py
+++ promptproc/monitor/views.py
@@ -32,49 +32,35 @@ except ImportError:
 
 #########################################################    
 def pilots(request):
-    pilot_uuid	= request.GET.get('uuid','')
-    pilot_pk	= request.GET.get('pk','')
+    return data_handler(request, 'pilots')
+
+#########################################################    
+def jobs(request):
+    return data_handler(request, 'jobs')
+
+#########################################################    
+def data_handler(request, what):
+    uuid	= request.GET.get('uuid','')
+    pk		= request.GET.get('pk','')
 
     now		= datetime.datetime.now()
     domain	= request.get_host()
     d=dict(domain=domain, time=str(now))
-    po = pilot.objects
-    if(pilot_uuid == '' and pilot_pk == ''):
-        d['pilots'] = po.all()
 
-    if(pilot_uuid != ''):
-        d['pilots'] = po.filter(uuid=pilot_uuid)
+    objects = None
+    if(what=='pilots'):
+        template = 'pilots.html'
+        objects = pilot.objects
         
-    if(pilot_pk != ''):
-        d['pilots'] = po.filter(pk=pilot_pk)
+    if(what=='jobs'):
+        template = 'jobs.html'
+        objects = job.objects
         
-    return render(request, 'pilots.html', d)
-    
-#    return HttpResponse(data)
+    if(uuid == '' and pk == ''):	d[what] = objects.all()
+    if(uuid != ''):			d[what] = objects.filter(uuid=uuid)
+    if(pk != ''):			d[what] = objects.filter(pk=pk)
+        
+    return render(request, template, d)
+#########################################################    
 # for later:
 #    data = serializers.serialize('json', [ p, ])
-###################################################
-#def data_handler(request):
-###################################################
-def jobs(request):
-    job_id = request.GET.get('job','')
-    latest = request.GET.get('latest','')
-    if(latest!=''):
-        add()
-        j = job.objects.latest(latest)
-        return HttpResponse("Job %s" % j.uuid)
-
-    if(job_id == ''):
-        now = datetime.datetime.now()
-        return render(request, 'jobs.html',
-                      {
-                          'jobs': job.objects.all(),
-                          'time': str(now)
-                      }
-        )
-
-
-    print(job_id)
-    ts = job.objects.get(id=job_id).ts_def
-    print(ts)
-    return HttpResponse("You're looking at job %s." % job_id)
