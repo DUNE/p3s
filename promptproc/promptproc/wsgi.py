31b710e5559a59d87ad398f80c987964767d03b4
Maxim Potekhin <potekhin@bnl.gov>
2017-01-20 23:46:09 +0100

    Updated wsgi.py to run on Ubuntu Apache - a bit hacky but will have to do for the moment, as Django 1.10 documentation is not very helpful, the recipe for path definition appears to not work.

--- promptproc/promptproc/wsgi.py
+++ promptproc/promptproc/wsgi.py
@@ -8,6 +8,10 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+sys.path.append("/home/maxim/projects/p3s/promptproc/")
+sys.path.append("/home/maxim/projects/p3s/promptproc/promptproc/")
 
 from django.core.wsgi import get_wsgi_application
 
