98cef5a6e10324cd5861a8ceba30cae0df27cfa3
Maxim Potekhin <potekhin@bnl.gov>
2017-01-20 11:09:37 +0100

    Python3 -> 3.5

--- promptproc/manage.py
+++ promptproc/manage.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.5
 ####################################################################
 #   THIS FILE HAS BEEN CUSTOMIZED, KEEP IT IN THE REPO   -mxp-     #
 #   For example:                                                   #
