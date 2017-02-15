069b10efcbcece3773f25fee8df213d9442e6baa
Maxim Potekhin <potekhin@bnl.gov>
2017-02-15 16:57:07 -0500

    Changes necessary for Apache deployment

--- promptproc/promptproc/settings.py
+++ promptproc/promptproc/settings.py
@@ -31,7 +31,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'y!m33zq-79&%-!r=e!&+jd2cq&ca85e(4q-9dsgsb4xx)g5p*+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+#DEBUG = True
 
 # -mxp- Please note this is for strictly development enviroment
 # and wiill be meaningless elsewhere
@@ -85,6 +86,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.static',
             ],
         },
     },
