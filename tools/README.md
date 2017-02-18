7cde341d1fcdbbe99349d93a95827f91862ce8d2
Maxim Potekhin <potekhin@bnl.gov>
2017-02-17 19:45:46 -0500

    Added README, improved graphics functionality in nx_digraph

---
+++ tools/README.md
@@ -0,0 +1,4 @@
+ # TOOLS
+
+An assortment of useful scripts etc to help develop and debug p3s.
+They usually graduate from the "sandbox" section
--- tools/nx_digraph.py
+++ tools/nx_digraph.py
@@ -21,18 +21,24 @@ parser.add_argument("-p", "--print",	action='store_true',
                     help="output the graph to stdout as graphml")
 
 parser.add_argument("-P", "--plot",	action='store_true',
-                    help="plot the graph using tk and matplotlib")
+                    help="plot the graph on screen using tk and matplotlib (if installed)")
+
+parser.add_argument("-s", "--save",	type=str,	default='',
+                    help="save the graph to this PNG file; requires --plot")
 
 parser.add_argument("-j", "--json",	action='store_true',
                     help="output the graph to stdout as json link and adjacency data")
+
 ########################### Parse all arguments #########################
 args	= parser.parse_args()
 graphml	= args.graphml
 prGraph	= args.print
 plot	= args.plot
+save	= args.save
 multi	= args.multi
 json	= args.json
 out	= args.out
+
 
 if(multi):
     g = nx.MultiDiGraph()
@@ -49,25 +55,28 @@ if(multi):
     e["resistance"] = "futile"
 
 else:
-    g = nx.DiGraph()
+    if(graphml!=''):
+        g = nx.read_graphml(graphml)
+    else:
+        g = nx.DiGraph()
 
-    g.add_node("foo")
-    g.node['foo']['voltage']=220
-    g.add_node("moo")
-    g.add_node("goo")
-    g.add_node("shoo")
+        g.add_node("foo")
+        g.node['foo']['voltage']=220
+        g.add_node("moo")
+        g.add_node("goo")
+        g.add_node("shoo")
 
-    g.add_edge("foo","moo")
-    g.add_edge("foo","goo")
-    g.add_edge("goo","shoo")
-    g.add_edge("moo","shoo")
+        g.add_edge("foo","moo")
+        g.add_edge("foo","goo")
+        g.add_edge("goo","shoo")
+        g.add_edge("moo","shoo")
 
-    efm = g.edge["foo"]["moo"]
-    efm['resistance']=10
-    g.node['foo']['voltage']=220
-    g.node['foo']['current']=10
-    g.node['moo']['current']=5
-    g.node['moo']['voltage']=110
+        efm = g.edge["foo"]["moo"]
+        efm['resistance']=10
+        g.node['foo']['voltage']=220
+        g.node['foo']['current']=10
+        g.node['moo']['current']=5
+        g.node['moo']['voltage']=110
 
     
 
@@ -88,7 +97,7 @@ if(prGraph):
     print(s)
 
 
-if(plot):
+if(plot or save!=''):
     try:
         import matplotlib.pyplot as plt
     except:
@@ -97,5 +106,9 @@ if(plot):
 
     
     nx.draw_networkx(g)
-    plt.show()
+    plt.axis('off')
+    if(save!=''):
+        plt.savefig(save)
+    if(plot):
+        plt.show()
     
