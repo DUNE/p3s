76ea591039fbd13294ccc08554c182801682a112
Maxim Potekhin <potekhin@bnl.gov>
2017-03-08 21:51:30 -0500

    Figured that I need to quote the args

--- clients/pgen.sh
+++ clients/pgen.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-# Simple pilot generator to start multiple pilots on a single WN
+# Simple pilot generator to start multiple pilots on a single WN.
+# To preserve the command line, use something like \'-S http://serenity.local:80/\'
+
 DIR=`dirname "$(readlink -f "$0")"`
 N=1
 if [ -n "$1" ];
@@ -13,5 +15,5 @@ fi
 
 for i in `seq 1 $N`;
 do
-$DIR/pilot.py $2
+eval $DIR/pilot.py $2
 done
