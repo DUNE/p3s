ec785b4a410e78727f7f725ddde78a5365a3fbfc
Maxim Potekhin <potekhin@bnl.gov>
2017-04-10 11:37:00 -0400

    Simple improvement in the setup script.

--- configuration/p3s_setup.sh
+++ configuration/p3s_setup.sh
@@ -1,9 +1,27 @@
 #!/bin/sh
 
-# this will need to be edited for deployment and most testing,
-# so conisder this a template for the environment
+# setting up the p3s environment
+
 p3s_server="serenity.local"
-echo setting $p3s_server on port $1
-export P3S_SERVER="http://$p3s_server:$1/"
+port=8000
+
+if [ -z "$1" ]
+  then
+      echo "Using default server"
+else
+    p3s_server=$1
+fi
+
+if [ -z "$2" ]
+  then
+      echo "Using default port"
+else
+    port=$2
+fi
+
+echo setting $p3s_server on port $port
+exit
+
+export P3S_SERVER="http://$p3s_server:$port/"
 export P3S_VERBOSITY=2
-alias p3s="./manage.py runserver 0.0.0.0:$1"
+export P3S_DIRPATH=/home/maxim/p3sdata/ # example: for cases where I/O is in one directory
