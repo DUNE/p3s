66bd19f8538b8868392e91662b67e05493f58956
Maxim Potekhin <potekhin@bnl.gov>
2017-04-14 13:54:44 -0400

    Simple client to print summary

---
+++ clients/summary.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3.5
+import argparse
+import json
+
+from comms import logfail
+from serverAPI import serverAPI
+from clientenv import clientenv
+
+#########################################################################        
+#############################  BEGIN  ###################################
+
+(user, server, verb, site) = clientenv()
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-S", "--server",	type=str,	default=server,
+                    help="the server address, defaults to $P3S_SERVER or if unset to http://localhost:8000/")
+
+
+########################### Parse all arguments #########################
+args = parser.parse_args()
+
+server =  args.server
+
+API  = serverAPI(server=server)
+resp = API.get2server('info','dash', '?out=json')
+info = json.loads(resp)
+
+print("Pilots: total %s, idle %s, running %s, stopped %s" %
+      (info['pilots']['data'][0], info['pilots']['data'][1], info['pilots']['data'][2], info['pilots']['data'][3]))
+print("Jobs: total %s, defined %s, running %s, finished %s" %
+      (info['jobs']['data'][0],   info['jobs']['data'][1],   info['jobs']['data'][2],   info['jobs']['data'][3]))
+
+print("Workflows %s" % info['workflows']['data'][0])
+print("Datasets %s" % info['datasets']['data'][0])
