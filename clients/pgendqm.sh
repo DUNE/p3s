4e44a08fb66618307d42e3ce917d9022a8792fa0
Maxim Potekhin <potekhin@bnl.gov>
2017-05-03 22:24:40 +0200

    Updates for NEUTDQM

--- clients/pgendqm.sh
+++ clients/pgendqm.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
+export P3S_SERVER="http://neutdqm:80/"
+
+
+# We keep this as a commment since the infomation will actually be provided
+# by the server and the pilot pulls is via the "s" optio
+# This is for reference, since once again 
 # Simple pilot generator to start multiple pilots on a single WN.
 # To preserve the command line, use something like \'-S http://serenity.local:80/\'
 
-export P3S_SERVER="http://neutdqm:80/"
-export P3S_SITE="neutdqm"
+#export P3S_SITE="neutdqm"
 
-export P3S_VERBOSITY=2
-export P3S_DIRPATH="/home/maxim/p3sdata/"
+#export P3S_VERBOSITY=2
+#export P3S_DIRPATH="/mnt/nas01/users/mxp/p3sdata/"
 
 
 DIR=`dirname "$(readlink -f "$0")"`
@@ -22,6 +27,5 @@ fi
 line=''
 for i in `seq 1 $N`;
 do
-#    line="$DIR/pilot.py $2 &" $line
-eval $DIR/pilot.py $2 &
+eval $DIR/pilot.py -s $P3S_SITE &
 done
