c4f67575411fa8d6d4287e65c4f5148cf39c358e
Maxim Potekhin <mxp@neutdqm06.cern.ch>
2017-05-04 02:00:24 +0200

    Stubbed out an additional lar payload definition

---
+++ inputs/larsoft/neutdqm_lar_2.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
+source /cvmfs/fermilab.opensciencegrid.org/products/larsoft/setups
+
+cd /mnt/nas01/software/dqm/larsoft/v06
+source localProducts_larsoft_v06_33_00_e14_prof/setup
+mrbslp
+cd /mnt/nas01/users/mxp/
+
+lar -c /mnt/nas01/software/dqm/larsoft/v06/job/onlinemonitorprotodune.fcl /mnt/nas01/users/radescu/Feb2017_v22/inputs/detsim_single_DistONSuppOFF_100.root -n1
