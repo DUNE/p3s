3046757ade75f3601c0775505bb0b0f7f878657a
Maxim Potekhin <mxp@neutdqm.cern.ch>
2017-05-04 17:17:26 +0200

    -T option for lar

--- inputs/larsoft/neutdqm_lar_2.sh
+++ inputs/larsoft/neutdqm_lar_2.sh
@@ -7,4 +7,4 @@ source localProducts_larsoft_v06_33_00_e14_prof/setup
 mrbslp
 cd /mnt/nas01/users/mxp/
 
-lar -c /mnt/nas01/users/mxp/projects/p3s/inputs/larsoft/neutdqm_lar_2.fcl /mnt/nas01/users/radescu/Feb2017_v22/inputs/detsim_single_DistONSuppOFF_100.root -o foo.root -n1
+lar -c /mnt/nas01/users/mxp/projects/p3s/inputs/larsoft/neutdqm_lar_2.fcl /mnt/nas01/users/radescu/Feb2017_v22/inputs/detsim_single_DistONSuppOFF_100.root -T foo.root -n1
