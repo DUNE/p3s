64a0dc8b12ad9176d3f20a06b6e76cb6f2d9bdbb
Maxim Potekhin <potekhin@bnl.gov>
2017-06-06 13:10:15 -0400

    tweak the env test

--- inputs/larsoft/dirtest_1.sh
+++ inputs/larsoft/dirtest_1.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+echo Sourcing
+source/afs/cern.ch/user/m/mxp/vp3s/bin/activate
+python -V
+source/afs/cern.ch/user/m/mxp/projects/p3s/configuration/p3s_setup.sh
+
 hostname
 date
 env | grep P3S
--- inputs/larsoft/lxdqm_lar_4.sh
+++ inputs/larsoft/lxdqm_lar_4.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/bash
+
+# set up larsoft/duneTPC
 source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
 source /cvmfs/fermilab.opensciencegrid.org/products/larsoft/setups
-source/afs/cern.ch/user/m/mxp/projects/p3s/configuration/p3s_setup.sh
 
 cd /eos/experiment/neutplatform/protodune/groupdisk/software/ds/
 source localProducts_larsoft_v06_38_00_e14_prof/setup
 mrbslp
+
+# set up p3s
+source/afs/cern.ch/user/m/mxp/vp3s/bin/activate
+source/afs/cern.ch/user/m/mxp/projects/p3s/configuration/p3s_setup.sh
 
 cd $P3S_OUTPUT
 p=$$
