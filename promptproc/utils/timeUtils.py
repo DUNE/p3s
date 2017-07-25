1da0114f83b511581e157d1f62accb07daf3f3b4
Maxim Potekhin <potekhin@bnl.gov>
2017-07-25 15:52:01 -0400

    Inlcude all fields in uptime

--- promptproc/utils/timeUtils.py
+++ promptproc/utils/timeUtils.py
@@ -43,8 +43,8 @@ def dt(stringTime):
 
 def uptime():
     with open('/proc/uptime', 'r') as f:
-        uptime_seconds = float(f.readline().split()[0])
-        uptime_string = str(timedelta(seconds = uptime_seconds))
-
-        return uptime_string
+        return f.readline()
+#        uptime_seconds = float(f.readline().split()[0])
+#        uptime_string = str(timedelta(seconds = uptime_seconds))
+#        return uptime_string
 
