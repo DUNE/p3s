d02a3c1ad967b6d63e96af64f018c2d7521935e8
Maxim Potekhin <potekhin@bnl.gov>
2017-09-01 15:35:07 -0400

    Cleared gargabge out of the initial setup scrupt

--- configuration/sagebrush.sh
+++ configuration/sagebrush.sh
@@ -2,55 +2,13 @@
 
 # setting up the p3s environment
 
-# Default server/site:
-p3s_server="serenity.local"
-p3s_site="sagebrush"
-p3s_dirpath="/home/maxim/p3sdata/"
-# override for neutdqm:
+p3s_dirpath=/home/maxim/p3sdata/
 
-h=`hostname`
-port=80
-
-echo Setting up environment on host $h
-if [[ $h == neutdq* ]];
-then
-    p3s_server="neutdqm"
-    p3s_site="neutdqm"
-    p3s_dirpath="/mnt/nas01/users/mxp/p3sdata/"
-    port=80
-fi 
-
-# FIXME - below will work for testing on lxplus
-# but hostname will be different on lxbatch, caveat emptor
-if [[ $h == lx* ]];
-then
-    p3s_server="p3s-web"
-    p3s_site="lxvm"
-    p3s_dirpath=""
-    port=8000
-fi 
-
-
-if [ -z "$1" ]
-  then
-      echo "Using default server" $p3s_server
-else
-    p3s_server=$1
-fi
-
-if [ -z "$2" ]
-  then
-      echo "Using default port" $port
-else
-    port=$2
-fi
-
-
-
-export P3S_SERVER="http://$p3s_server:$port/"
-export P3S_SITE=$p3s_site
-
-export P3S_VERBOSITY=2
+export P3S_SERVER="http://serenity.local/"
+export P3S_SITE='sagebrush'
+export P3S_PILOTS=12
+export P3S_VERBOSITY=0
 export P3S_DIRPATH=$p3s_dirpath
-
+export P3S_PILOTLOG=/tmp/pilotlog
+export P3S_JOBLOG=/tmp/joblog
 env | grep P3S
