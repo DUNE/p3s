8bb8297c039b157d09a61ecd7aba395514e0da6a
Maxim Potekhin <potekhin@bnl.gov>
2017-09-12 20:50:41 -0400

    Added the pur model and its admin

--- display/purity/admin.py
+++ display/purity/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import pur
 
-# Register your models here.
+############
+class purAdmin(admin.ModelAdmin):
+    list_display = ('run', 'tpc', 'ts', 'lifetime', 'error', 'count')
+    empty_value_display = '-empty-'
+    
+admin.site.register(pur, purAdmin)
+
--- display/purity/models.py
+++ display/purity/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+import json
+from django.db		import models
+from django.core	import serializers
 
-# Create your models here.
+
+class pur(models.Model):
+    run		= models.PositiveIntegerField(default=0)
+    tpc		= models.PositiveIntegerField(default=0)
+    ts		= models.DateTimeField('ts', blank=True, null=True)
+    lifetime	= models.FloatField('lifetime', default=0.0)
+    error	= models.FloatField('error', default=0.0)
+    count	= models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return serializers.serialize("json", [self, ])
