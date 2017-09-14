2405cc993778c3e72e4173840dd70fd7f7c9d1be
Maxim Potekhin <potekhin@bnl.gov>
2017-09-13 20:15:13 -0400

    Tweaks in monitor

--- display/monitor/views.py
+++ display/monitor/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts	import render
+from django.http	import HttpResponse
 
-# Create your views here.
+import  django_tables2 as tables
+from	django_tables2			import RequestConfig
+from	django_tables2.utils		import A
+
+from purity.models import pur
+
+#########################################################    
+# general request handler for summary type of a table
+def data_handler(request, what):
+    # testing only
+    objs = pur.objects.all()
+
+    d = {}
+    
+    t = tables.Table(objs)
+    RequestConfig(request).configure(t)
+
+    d['table'] = t
+    return render(request, 'monitor_base.html', d)
+
+    #return HttpResponse('requested: '+str(len(objs)))
+
