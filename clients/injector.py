b67551647593f3170dbef40285c8e95e9c02fe0f
Maxim Potekhin <potekhin@bnl.gov>
2017-09-21 18:22:12 -0400

    A stub for "injector" after a little cleanup

--- clients/injector.py
+++ clients/injector.py
@@ -20,6 +20,8 @@ from django.conf	import settings
 from django.utils	import timezone
 
 from serverAPI import serverAPI
+from clientenv import clientenv
+
 #########################################################
 settings.configure(USE_TZ = True) # see the above note on TZ
 
@@ -38,35 +40,24 @@ class Dataset(dict):
         self['wf']	= wf
         
 #########################################################################
+envDict = clientenv(outputDict=True) # Will need ('server', 'verb'):
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-a", "--add",	type=str,	default='',
-                    help='''Add a workflow. Argument is the name of the prototype DAG (already stored on the server).
-                    If no special name is provided for the workflow via the *name* argument,
-                    defaults to the name of the parent DAG''')
-
 parser.add_argument("-S", "--server",	type=str,	default='http://localhost:8000/',
                     help="the server address, defaults to http://localhost:8000/")
-
-parser.add_argument("-U", "--usage",	action='store_true',
-                    help="print usage notes and exit")
-
-parser.add_argument("-l", "--logdir",	type=str,	default=logdefault,
-                    help="Log directory (defaults to "+logdefault+"). The file name in logdir will be 'injector.log'")
-
-parser.add_argument("-t", "--test",	action='store_true',
-                    help="when set, forms a request but does not contact the server")
-
-parser.add_argument("-v", "--verbosity",	type=int,
-                    default=0, choices=[0, 1, 2],
-                    help="increase output verbosity")
 
 parser.add_argument("-c", "--cycles",	type=int,	default=1,
                     help="how many cycles (with period in seconds) to stay alive")
 
 parser.add_argument("-p", "--period",	type=int,	default=5,
                     help="period of the pilot cycle, in seconds")
+
+
+parser.add_argument("-v", "--verbosity",	type=int, default=envDict['verb'], choices=[0, 1, 2],
+                    help="set output verbosity")
+
+parser.add_argument("-t", "--test",	action='store_true',	help="print parameters and exit")
 
 # --
 parser.add_argument("-f", "--filename",	type=str,	default='', help="file to watch")
@@ -76,33 +67,36 @@ args = parser.parse_args()
 
 # strings
 server	= args.server
-logdir	= args.logdir
-filename = args.filename
+cycles	= args.cycles
+period	= args.period
+test	= args.test
+
+filename= args.filename
 datadir = args.datadir
 
-add	= args.add
 
 # misc
 verb	= args.verbosity
-usage	= args.usage
 
 # scheduling
 period	= args.period
 cycles	= args.cycles
+verb	= args.verbosity
 
 # testing (pre-emptive exit with print)
 tst	= args.test
+
+
+if(tst):
+    print('server:',server,'cycles:',cycles)
 
 
 
 
 API = serverAPI(server=server)
 
-###################### USAGE REQUESTED? ################################
-if(usage):
-    print(Usage)
-    exit(0)
 
+logdir = '/tmp' # FIXME
 ################### BEGIN: PREPARE LOGGER ##############################
 # Check if we have a log directory, example: /tmp/p3s/. Create if not
 if(not os.path.exists(logdir)):
