95583ed2d1865601a677fbd211e11509bae7cb1d
Maxim Potekhin <potekhin@bnl.gov>
2017-09-26 18:52:10 -0400

    Improve the site layout

--- configuration/lxvm.sh
+++ configuration/lxvm.sh
@@ -8,8 +8,16 @@ export P3S_SERVER="http://p3s-web:80/"
 export P3S_SITE='lxvm'
 export P3S_PILOTS=300
 export P3S_PILOT_TO=3000
+export P3S_PILOT_MAXRUNTIME=90000
 export P3S_VERBOSITY=0
+#
 export P3S_DIRPATH=$p3s_dirpath
+#
 export P3S_PILOTLOG=$P3S_DIRPATH/pilotlog
 export P3S_JOBLOG=$P3S_DIRPATH/joblog
+#
+export P3S_CONDOR_OUTPUT=$P3S_DIRPATH/condorOutput
+export P3S_CONDOR_ERROR=$P3S_DIRPATH/condorError
+export P3S_CONDOR_LOG=$P3S_DIRPATH/condorLog
+
 env | grep P3S
