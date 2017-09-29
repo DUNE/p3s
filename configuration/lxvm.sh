ea1602eb4381e4ff56f9412179b44aaad3f13d59
Maxim Potekhin <potekhin@bnl.gov>
2017-09-29 15:39:08 -0400

    Added the lifetime for the data

--- configuration/lxvm.sh
+++ configuration/lxvm.sh
@@ -2,8 +2,6 @@
 
 # setting up the p3s environment
 
-p3s_dirpath=/eos/experiment/neutplatform/protodune/np04tier0/p3s
-
 export P3S_SERVER="http://p3s-web:80/"
 export P3S_SITE='lxvm'
 export P3S_PILOTS=300
@@ -11,7 +9,7 @@ export P3S_PILOT_TO=3000
 export P3S_PILOT_MAXRUNTIME=90000
 export P3S_VERBOSITY=0
 #
-export P3S_DIRPATH=$p3s_dirpath
+export P3S_DIRPATH=/eos/experiment/neutplatform/protodune/np04tier0/p3s
 #
 export P3S_PILOTLOG=$P3S_DIRPATH/pilotlog
 export P3S_JOBLOG=$P3S_DIRPATH/joblog
@@ -21,3 +19,7 @@ export P3S_CONDOR_ERROR=$P3S_DIRPATH/condorError
 export P3S_CONDOR_LOG=$P3S_DIRPATH/condorLog
 
 env | grep P3S
+
+export DQM_DATA_LIFE=1200 # in minutes, e.g. 24 hrs will be 1440
+
+env | grep DQM
