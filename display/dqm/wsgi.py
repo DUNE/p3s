79cf6e2cb5389c9baa1949a67b8c560806ce38a1
Maxim Potekhin <potekhin@bnl.gov>
2017-10-04 14:36:27 -0400

    a tweak

--- display/dqm/wsgi.py
+++ display/dqm/wsgi.py
@@ -8,6 +8,10 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+sys.path.append("/home/mxp/projects/p3s/display/")
+sys.path.append("/home/maxim/projects/p3s/display/dqm/")
 
 from django.core.wsgi import get_wsgi_application
 
