432c1ab8e8a8995321c7d2fba6c9f8d738b4df4a
Maxim Potekhin <potekhin@bnl.gov>
2017-10-09 21:09:58 -0400

    reduce number of pilots for now

--- configuration/lxvm.sh
+++ configuration/lxvm.sh
@@ -4,7 +4,7 @@
 
 export P3S_SERVER="http://p3s-web:80/"
 export P3S_SITE='lxvm'
-export P3S_PILOTS=300
+export P3S_PILOTS=50
 export P3S_PILOT_TO=3000
 export P3S_PILOT_MAXRUNTIME=90000
 export P3S_VERBOSITY=0
