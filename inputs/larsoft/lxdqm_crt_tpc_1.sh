dcf58554318763a57d2798b7d9c96eee650868dc
Maxim Potekhin <potekhin@bnl.gov>
2017-10-23 19:33:33 -0400

    Correct some paths in the script

--- inputs/larsoft/lxdqm_crt_tpc_1.sh
+++ inputs/larsoft/lxdqm_crt_tpc_1.sh
@@ -12,7 +12,7 @@ mrbsetenv
 # set up the virtual environment
 source ${P3S_VENV}/bin/activate
 
-cd $P3S_OUTPUT
+cd $P3S_DATA/output
 # job uuid is set by the pilot
 tmpdir=$P3S_JOB_UUID
 mkdir $tmpdir
@@ -23,6 +23,6 @@ cp $P3S_CRT_TXT .
 env | grep P3S
 lar -c $P3S_FCL $P3S_INPUT -T $P3S_OUTPUT/$tmpdir/lxdqm_crt_tpc_1.root
 # -n$P3S_NEVENTS
-cp $P3S_OUTPUT/joblog/$P3S_JOB_UUID.out .
-cp $P3S_OUTPUT/joblog/$P3S_JOB_UUID.err .
+cp $P3S_DATA/joblog/$P3S_JOB_UUID.out .
+cp $P3S_DATA/joblog/$P3S_JOB_UUID.err .
 exit
