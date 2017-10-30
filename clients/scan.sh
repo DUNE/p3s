c600bb0cfad6175e5acf7f8780b6296769657212
Maxim Potekhin <potekhin@bnl.gov>
2017-10-30 14:26:14 -0400

    small additions to scan

--- clients/scan.sh
+++ clients/scan.sh
@@ -1,4 +1,23 @@
 #!/bin/sh
 
+if [ ! -d "$1" ]; then
+    echo Directory $1 not found, exiting
+    exit 0
+fi
 
-find $1 -maxdepth 1 -mindepth 1 -mmin -10 | sed 's/\.\///'
+if [ -z "$2" ]; then
+    echo "No time window defined"
+    exit 0
+fi
+
+# -- source /afs/cern.ch/user/m/mxp/vp3s/bin/activate
+
+export $P3S_HOME=/home/maxim/projects/p3s/
+
+$p=`$P3S_HOME/clients/summary.py -p`
+
+cd $1
+d=`pwd`
+echo Directory: $d
+echo Files:
+find . -maxdepth 1 -mindepth 1 -mmin $2 | sed 's/\.\///'
