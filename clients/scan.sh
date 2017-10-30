64dc99c2ca97e88ee28cdc8c6f733150867f23fe
Maxim Potekhin <potekhin@bnl.gov>
2017-10-29 21:05:24 -0400

    End-of-day check

--- clients/scan.sh
+++ clients/scan.sh
@@ -1,20 +1,4 @@
 #!/bin/sh
 
-# Simple pilot generator to start multiple pilots on a single WN.
-# To preserve the command line, use something like \'-S http://serenity.local:80/\'
 
-DIR=`dirname "$(readlink -f "$0")"`
-N=1
-if [ -n "$1" ];
-then
-    if [ "$1" -eq "$1" ] 2>/dev/null; then
-	N=$1
-    fi
-fi
-
-line=''
-for i in `seq 1 $N`;
-do
-#    line="$DIR/pilot.py $2 &" $line
-eval $DIR/pilot.py $2 &
-done
+find $1 -maxdepth 1 -mindepth 1 -mmin -10 | sed 's/\.\///'
