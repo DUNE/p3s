2a34c49259b28ecd7e86d481f98ec86eda8d20a1
Maxim Potekhin <potekhin@bnl.gov>
2017-11-02 18:28:55 +0100

    Small tweak in "summary", simplifying "scan"

--- clients/scan.sh
+++ clients/scan.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 Nargs=$#
 
-if [ ! $Nargs -eq 3 ]; then
-    echo Wrong number of arguments - expecting 3 - exiting...
+if [ ! $Nargs -eq 2 ]; then
+    echo Wrong number of arguments - expecting 2 - exiting...
     exit
 fi
 
@@ -11,34 +11,38 @@ if [ "${P3S_SITE}" = 'lxvm' ]
 then
     echo Activating venv at lxvm
     source /afs/cern.ch/user/m/mxp/vp3s/bin/activate
+    export P3S_HOME=/afs/cern.ch/user/m/mxp/projects/p3s
+    export P3S_INPUT_DIR=$P3S_DIRPATH/input
+else
+    echo Assuming site sagebrush
+    export P3S_HOME=/home/maxim/projects/p3s
+    export P3S_INPUT_DIR=$P3S_DIRPATH/p3sdata/input
 fi
-
 # exit 0
 
 
 
-if [ ! -d "$1" ]; then
-    echo Directory $1 not found, exiting
-    exit 0
-fi
+#if [ ! -d "$1" ]; then
+#    echo Directory $1 not found, exiting
+#    exit 0
+#fi
 
-if [ -z "$2" ]; then
+if [ -z "$1" ]; then
     echo "No time window defined"
     exit 0
 fi
 
 
-export P3S_HOME=/home/maxim/projects/p3s
 
-cd $1
+cd $P3S_INPUT_DIR
 d=`pwd`
 echo Directory: $d
-files=`find . -maxdepth 1 -mindepth 1 -mmin $2 | sed 's/\.\///'`
+files=`find . -maxdepth 1 -mindepth 1 -mmin $1 | sed 's/\.\///'`
 echo Files:$files
 echo ${#files[@]}
 for f in $files
 do
     echo ! $f
-    echo $P3S_HOME/clients/dataset.py -g -i $d -f $f -J $P3S_HOME/inputs/jobs/$3
-    $P3S_HOME/clients/dataset.py -g -i $d -f $f -J $P3S_HOME/inputs/jobs/$3
+    echo $P3S_HOME/clients/dataset.py -g -i $d -f $f -J $P3S_HOME/$2
+    $P3S_HOME/clients/dataset.py -g -i $d -f $f -J $P3S_HOME/$2
 done
