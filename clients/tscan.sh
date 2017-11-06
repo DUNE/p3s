475bd71ac8ccb82ed30378663a8d26951e25f58f
buddhasystem <potekhin@bnl.gov>
2017-11-06 14:50:05 +0100

    Made the tscan script less chatty.

--- clients/tscan.sh
+++ clients/tscan.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+export P3S_HOME=/afs/cern.ch/user/m/mxp/projects/p3s
+export DQM_HOME=/afs/cern.ch/user/m/mxp/projects/dqmconfig
+
+source $P3S_HOME/configuration/lxvm.sh > /dev/null
+
 Nargs=$#
 
 if [ ! $Nargs -eq 2 ]; then
@@ -10,34 +15,23 @@ if [ ! $Nargs -eq 2 ]; then
 fi
 
 
-if [ "${P3S_SITE}" = 'lxvm' ]
-then
-    echo Activating venv at lxvm
-    source /afs/cern.ch/user/m/mxp/vp3s/bin/activate
-    export P3S_HOME=/afs/cern.ch/user/m/mxp/projects/p3s
-    export P3S_INPUT_DIR=$P3S_DIRPATH/input
-else
-    echo Assuming site sagebrush
-    export P3S_HOME=/home/maxim/projects/p3s
-    export P3S_INPUT_DIR=$P3S_DIRPATH/p3sdata/input
-fi
-# exit 0
-
-
+# echo Activating venv at lxvm
+source /afs/cern.ch/user/m/mxp/vp3s/bin/activate
+export P3S_HOME=/afs/cern.ch/user/m/mxp/projects/p3s
+export P3S_INPUT_DIR=$P3S_DIRPATH/input
 
 cd $P3S_INPUT_DIR
 d=`pwd`
 echo Directory: $d
-files=`find . -maxdepth 1 -mindepth 1 -mmin $1 -name "$2*" | sed 's/\.\///'`
+files=`find . -maxdepth 1 -mindepth 1 -mmin $1 -size +1 -name "$2*" | sed 's/\.\///'`
 echo Files:$files
-# echo ${#files[@]}
 
-# exit 0
+# echo ${#files[@]}
 
 for f in $files
 do
-    echo ! $f
-    $P3S_HOME/clients/dataset.py -g -i $d -f $f -J $P3S_HOME/inputs/larsoft/lxdqm_evdisp_4.json -N
-    $P3S_HOME/clients/dataset.py -g -i $d -f $f -J $P3S_HOME/inputs/larsoft/lxdqm_crt_tpc_3.json -N
-    $P3S_HOME/clients/dataset.py -g -i $d -f $f -J $P3S_HOME/inputs/larsoft/lxdqm_purity_5.json
+#    echo ! $f
+    $P3S_HOME/clients/dataset.py -v 0 -g -i $d -f $f -J $P3S_HOME/inputs/larsoft/lxdqm_evdisp_4.json -N
+    $P3S_HOME/clients/dataset.py -v 0 -g -i $d -f $f -J $P3S_HOME/inputs/larsoft/lxdqm_crt_tpc_3.json -N
+    $P3S_HOME/clients/dataset.py -v 0 -g -i $d -f $f -J $P3S_HOME/inputs/larsoft/lxdqm_purity_5.json
 done
