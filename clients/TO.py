1dd23773e1629348dd23dafdac457799b4f1b514
Maxim Potekhin <potekhin@bnl.gov>
2017-11-13 21:37:10 -0500

    Updated the clientenv API to new style

--- clients/TO.py
+++ clients/TO.py
@@ -20,25 +20,33 @@ import socket
 from serverAPI import serverAPI
 from clientenv import clientenv
 
-(user, server, verb, site, pl, jl) = clientenv()
+envDict = clientenv(outputDict=True) # Will need ('server', 'verb'):
 
 myhost = socket.gethostname()
-
-### p3s interface defined here
-API  = serverAPI(server=server, verb=0)
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument("-t", "--timeout",	type=int,	default=600,
                     help="pilot timeout in seconds, default 600")
 
+parser.add_argument("-S", "--server",	type=str,
+                    help="server URL: defaults to $P3S_SERVER or if unset to http://localhost:8000/",
+                    default=envDict['server'])
 
-# parser.add_argument("-T", "--test",	action='store_true', help="when set, parses input but does not contact the server")
+parser.add_argument("-v", "--verbosity",	type=int, default=envDict['verb'], choices=[0, 1, 2],
+                    help="set output verbosity")
+
+parser.add_argument("-T", "--test",	action='store_true', help="when set, parses input but does not contact the server")
 
 
 args	= parser.parse_args()
 to	= args.timeout
-# tst	= args.test
+verb	= args.verbosity
+server	= args.server
+tst	= args.test
+
+### p3s interface defined here
+API  = serverAPI(server=server, verb=0)
 
 
 resp = API.post2server('logic', 'pilotTO', dict(to=to, host=myhost))
