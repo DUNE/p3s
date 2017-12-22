13a39bdd73acb89c62bd56189d753549b8a9ac7b
Maxim Potekhin <potekhin@bnl.gov>
2017-12-22 10:55:57 -0500

    tweak

--- inputs/larsoft/lxdqm_crt_tpc_3.sh
+++ inputs/larsoft/lxdqm_crt_tpc_3.sh
@@ -32,7 +32,7 @@ then
     echo P3S_JOB_UUID undefined, setting new value:
     export P3S_JOB_UUID=`uuid`
     echo $P3S_JOB_UUID
-    export after=">$P3S_DATA/joblog/$P3S_JOB_UUID.out 2>$P3S_DATA/joblog/$P3S_JOB_UUID.err"
+    export after="1>$P3S_DATA/joblog/$P3S_JOB_UUID.out 2>$P3S_DATA/joblog/$P3S_JOB_UUID.err"
 #    echo $after
 #    exit
 fi
