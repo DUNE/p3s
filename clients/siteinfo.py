0af87eb8a308798ea84cbe729e0fa755a104c648
Maxim Potekhin <potekhin@bnl.gov>
2018-01-08 19:30:30 -0500

    Bug fix

--- clients/siteinfo.py
+++ clients/siteinfo.py
@@ -31,21 +31,19 @@ from clientenv import clientenv
 
 settings.configure(USE_TZ = True)
 
-(user, server, verb, site, pl, jl) = clientenv()
+envDict = clientenv(outputDict=True) # Will need ('server', 'verb'):
 
 host	= socket.gethostname()
 parser	= argparse.ArgumentParser()
 
-parser.add_argument("-S", "--server",	type=str,
-                    help='server URL: defaults to $P3S_SERVER or if unset to '+server,
-                    default=server)
+parser.add_argument("-S", "--server",	type=str,	default=envDict['server'],
+                    help='server URL: defaults to $P3S_SERVER or if unset to '+envDict['server'])
 
-parser.add_argument("-v", "--verbosity",type=int,	default=verb, choices=[0, 1, 2], help="verbosity")
+parser.add_argument("-v", "--verbosity",type=int,	default=envDict['verb'], choices=[0, 1, 2], help="verbosity")
 
-parser.add_argument("-s", "--site",	type=str,	default='', help='''site info in JSON format,
-will be intrepreted as a JSON file name if contains .json''')
+parser.add_argument("-j", "--json",	type=str,	default='', help='name of the file containing site info in JSON format')
 
-parser.add_argument("-d", "--delete",	type=str,	default='', help='delete site info')
+parser.add_argument("-d", "--delete",	type=str,	default='', help='name of the site to be deleted')
 
 
 ########################### Parse all arguments #########################
@@ -54,36 +52,33 @@ args = parser.parse_args()
 server	= args.server
 delete	= args.delete
 verb	= args.verbosity
-site	= args.site
+jsite	= args.json
 
 ### p3s interface defined here
 API  = serverAPI(server=server)
 
-if(site!=''):
-
-    f = None
-    if('.json' in site):
-        try:
-            f = open(site, 'r')
-            content = f.read()
-            f.close()
-            if(verb>1): print(content)
-            site = content
-        except:
-            print('Problems reading input file %s' % site)
-            exit(-1)
-
-
-    print('Site:', site)
-    resp = API.post2server('site', 'defineURL', {'site':site})
-    
-    if(verb>1): print (resp)
-    exit(0)
-
-
 if(delete!=''):
     resp = API.post2server('site', 'deleteURL', {'site': delete})
     if(verb>0): print(resp)
+    exit(0)
 
-###################### GRAND FINALE ####################################
+if(jsite==''):
+    print('No input specified')
+    exit(-1)
+
+content = ''
+try:
+    f = open(jsite, 'r')
+    content = f.read()
+    f.close()
+    if(verb>1): print(content)
+except:
+    print('Problems reading input file %s' % site)
+    exit(-1)
+
+
+resp = API.post2server('site', 'defineURL', {'site': content})
+    
+if(verb>1): print (resp)
 exit(0)
+
