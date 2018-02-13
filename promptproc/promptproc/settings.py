692390852f8f4d491d98b8e42ca7bf6443ff5e67
Maxim Potekhin <potekhin@bnl.gov>
2018-02-13 12:15:02 -0500

    configure TZ per local preference

--- promptproc/promptproc/settings.py
+++ promptproc/promptproc/settings.py
@@ -134,7 +134,10 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'America/New_York'
+try:
+    TIME_ZONE = SITE['tz']
+except: # take default which is not guaranteed to be optimal...
+    TIME_ZONE = 'America/New_York'
 
 USE_I18N = True
 
