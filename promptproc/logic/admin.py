4fa34f3dcd861910da81d26eed5f6c108be54233
Maxim Potekhin <potekhin@bnl.gov>
2018-02-15 14:59:58 -0500

    Add user to "admin"

--- promptproc/logic/admin.py
+++ promptproc/logic/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 
 
-from .models import service
+from .models import service, user
 
 class serviceAdmin(admin.ModelAdmin):
     list_display = ('name', 'ts', 'info')
     empty_value_display = '-empty-'
     
 admin.site.register(service, serviceAdmin)
+
+class userAdmin(admin.ModelAdmin):
+    list_display = ('name')
+    empty_value_display = '-empty-'
+    
+admin.site.register(user, userAdmin)
 
