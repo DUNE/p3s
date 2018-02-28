6d7f881b11e5f7867d0b225eeb0d7d656367f5e1
Maxim Potekhin <potekhin@bnl.gov>
2018-02-28 14:56:00 -0500

    improve the test script

--- inputs/larsoft/test/hitdumptest.sh
+++ inputs/larsoft/test/hitdumptest.sh
@@ -4,7 +4,15 @@ source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
 setup dunetpc ${DUNETPCVER} -q ${DUNETPCQUAL}
 
 lar -n ${NUMEVENTS} -c dump_hits.fcl ${INPUTFILE}
-ofn=`basename ${INPUTFILE}`_DumpHits.log
+
+if [ -z ${P3S_JOB_UUID+x} ];
+then
+    echo P3S_JOB_UUID undefined, setting new value:
+    export P3S_JOB_UUID=`uuid`
+    echo $P3S_JOB_UUID
+fi
+
+ofn=`basename ${INPUTFILE}`_DumpHits_${P3S_JOB_UUID}.log
 
 mv DumpHits.log ${OUTPUTDIR}/${ofn}
 
