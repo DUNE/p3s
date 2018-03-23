81515a79feff1341da835621116d0c70b634dd16
Maxim Potekhin <potekhin@bnl.gov>
2018-03-23 16:42:06 -0400

    properly handle the job UUID like in LArSoft wrappers - in the pedestal scripts

--- inputs/larsoft/setup_env_np04dqm.sh
+++ inputs/larsoft/setup_env_np04dqm.sh
@@ -10,3 +10,9 @@ source /afs/cern.ch/user/n/np04dqm/public/vp3s/bin/activate
 
 export P3S_INPUT_DIR=$P3S_DIRPATH/input
 
+if [ -z ${P3S_JOB_UUID+x} ];
+then
+    echo P3S_JOB_UUID undefined, setting new value:
+    export P3S_JOB_UUID=`uuid`
+    echo $P3S_JOB_UUID
+fi
