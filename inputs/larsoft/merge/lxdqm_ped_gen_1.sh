5ac5d989b0888cfaf96ea3f455b6064c88f43af3
Maxim Potekhin <potekhin@bnl.gov>
2018-03-23 16:35:29 -0400

    Try to use the new setup

--- inputs/larsoft/merge/lxdqm_ped_gen_1.sh
+++ inputs/larsoft/merge/lxdqm_ped_gen_1.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-export P3S_HOME=/afs/cern.ch/user/n/np04dqm/public/p3s/p3s
-export DQM_HOME=/afs/cern.ch/user/n/np04dqm/public/p3s/dqmconfig
+#export P3S_HOME=/afs/cern.ch/user/n/np04dqm/public/p3s/p3s
+#export DQM_HOME=/afs/cern.ch/user/n/np04dqm/public/p3s/dqmconfig
+#source $P3S_HOME/configuration/lxvm_np04dqm.sh > /dev/null
+#source /afs/cern.ch/user/n/np04dqm/public/vp3s/bin/activate
 
-source $P3S_HOME/configuration/lxvm_np04dqm.sh > /dev/null
-
-source /afs/cern.ch/user/n/np04dqm/public/vp3s/bin/activate
+$P3S_HOME/inputs/larsoft/setup_env_np04dqm.sh
 
 export P3S_INPUT_DIR=$P3S_DIRPATH/input
 
