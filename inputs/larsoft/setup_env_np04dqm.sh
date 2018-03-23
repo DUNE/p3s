f56def1ec4efdf3e24265b084cc8a7298df86dbb
Maxim Potekhin <potekhin@bnl.gov>
2018-03-23 16:39:08 -0400

    fixed teh chicken and egg problem

--- inputs/larsoft/setup_env_np04dqm.sh
+++ inputs/larsoft/setup_env_np04dqm.sh
@@ -2,7 +2,6 @@
 
 # This is the common part across many payload scripts
 
-export P3S_HOME=/afs/cern.ch/user/n/np04dqm/public/p3s/p3s
 export DQM_HOME=/afs/cern.ch/user/n/np04dqm/public/p3s/dqmconfig
 
 source $P3S_HOME/configuration/lxvm_np04dqm.sh > /dev/null
