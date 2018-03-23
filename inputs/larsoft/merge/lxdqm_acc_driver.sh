54e90995a7e9f1422338049b685f7c1fc348191e
Maxim Potekhin <potekhin@bnl.gov>
2018-03-23 19:31:44 -0400

    finalizing the logic in the driver

--- inputs/larsoft/merge/lxdqm_acc_driver.sh
+++ inputs/larsoft/merge/lxdqm_acc_driver.sh
@@ -6,7 +6,7 @@ source $P3S_HOME/inputs/larsoft/setup_env_np04dqm.sh
 export P3S_INPUT_DIR=$P3S_DIRPATH/input
 
 if [ ! -d "$P3S_INPUT_DIR" ]; then
-    $P3S_HOME/clients/service.py -n acc_init -m "Problem with directory $P3S_INPUT_DIR"
+    $P3S_HOME/clients/service.py -n acc_init -m "Problem with the directory $P3S_INPUT_DIR"
     exit 1
 fi
 
@@ -18,32 +18,48 @@ files=`find . -maxdepth 1 -mindepth 1 -size +1 -name "ped*" | sed 's/\.\///'`
 
 COUNTER=0
 
-export MERGE_FACTOR=3
+export MERGE_FACTOR=5
 
 while [ $COUNTER -lt 150 ]; do
     files=`find . -maxdepth 1 -mindepth 1 -size +1 -name "ped*" | sed 's/\.\///'`
     for f in $files
     do
+	q=`$P3S_HOME/clients/dataset.py -G -f $f`
+
+	# check if this file was already accounted for, skip it then
+	if [ $q != "[]" ]; then continue; fi
+	
 	echo merging $f in $d
-	argument='{"name":"'$f'","state":"defined","comment":"testing","datatype":"MERGEINPUT","dirpath":"'$P3S_INPUT_DIR'"}'
+	    
 	# register this file with the appropriate client:
+	argument='{"name":"'$f'","state":"defined","comment":"testing","datatype":"MERGEINPUT","dirpath":"'$P3S_INPUT_DIR'"}'
 	$P3S_HOME/clients/dataset.py -r -j $argument
+	
 	# do the merge:
 	$P3S_HOME/tools/accumulator.exe add merge.root $f
+
+	# check if merged enough files
 	let COUNTER+=1
-	if [ $COUNTER -ge $MERGE_FACTOR ]; then break fi
+	if [ $COUNTER -ge $MERGE_FACTOR ]; then
+	    break
+	fi
     done
+
+    # if enough files were merged, just finalize the result and exit
     if [ $COUNTER -ge $MERGE_FACTOR ]; then
 	break
     fi
 done
+
 echo $COUNTER
 ts=`date -d "today" +"%Y%m%d%H%M"`
 mv merge.root ../merge/merge$ts.root
 
 # Dev notes:
-# If we used this to run jobs, it would result in an asynchronous operation of these jobs in p3s
+# If we used the line below  to run jobs, it would result in an asynchronous operation of these jobs in p3s
 # so the merge file could be in an inconsistent state.
+#
 # $P3S_HOME/clients/dataset.py -v 0 -g -i $d -f $f -J $P3S_HOME/inputs/larsoft/merge/lxdqm_acc_add_2.json
+#
 # For this reason we revert to a synchronous loop.
 
