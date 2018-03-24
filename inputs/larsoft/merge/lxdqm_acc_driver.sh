b82a123923357e28df2eab158a32a4764f4ae104
Maxim Potekhin <potekhin@bnl.gov>
2018-03-24 14:05:55 -0400

    Added some safeguards and moving used pedestal files to a separate folder

--- inputs/larsoft/merge/lxdqm_acc_driver.sh
+++ inputs/larsoft/merge/lxdqm_acc_driver.sh
@@ -5,16 +5,35 @@ source $P3S_HOME/inputs/larsoft/setup_env_np04dqm.sh
 
 export P3S_INPUT_DIR=$P3S_DIRPATH/input
 export P3S_MERGE_DIR=$P3S_DIRPATH/merge
+export P3S_PED_DIR=$P3S_DIRPATH/pedestals
 
 
 # declare this once!
 # AND fix the ugly later
 
-export MERGE_FILE="merge.root"
-export NCHAN=100
+if [ -z ${MERGE_FILE+x} ];
+then
+    export MERGE_FILE="merge.root"
+fi
+
+if [ -z ${NCHAN+x} ];
+then
+    export NCHAN=100
+fi
+
+if [ -z ${MERGE_FACTOR+x} ];
+then
+    export MERGE_FACTOR=5
+fi
+
+if [ -z ${SLEEP+x} ];
+then
+    export SLEEP=10
+fi
+
 
 if [ ! -d "$P3S_INPUT_DIR" ]; then
-    $P3S_HOME/clients/service.py -n acc_init -m "Problem with the directory $P3S_INPUT_DIR"
+    $P3S_HOME/clients/service.py -n merge_add -m "Problem with the directory $P3S_INPUT_DIR"
     exit 1
 fi
 
@@ -24,21 +43,18 @@ d=`pwd`
 
 # check if we have the merge file
 if [ ! -f "$MERGE_FILE" ]; then
-    echo NO MERGE FILE
+    echo $MERGE_FILE not detected, will be created
     $P3S_HOME/tools/accumulator.exe init $MERGE_FILE $NCHAN
-    $P3S_HOME/clients/service.py -n acc_init -m "$MERGE_FILE created with $NCHAN channels"
+    $P3S_HOME/clients/service.py -n merge_init -m "$MERGE_FILE created with $NCHAN channels"
 fi
 
 # detect potential files for merging
 files=`find . -maxdepth 1 -mindepth 1 -size +1 -name "ped*" | sed 's/\.\///'`
 
-# a counter of the files merged
+# a counter for the files that were merged
 COUNTER=0
 
-# will deal with this later
-export MERGE_FACTOR=5
-
-while [ $COUNTER -lt 150 ]; do
+while [ $COUNTER -lt $MERGE_FACTOR ]; do
     files=`find . -maxdepth 1 -mindepth 1 -size +1 -name "ped*" | sed 's/\.\///'`
     for f in $files
     do
@@ -55,7 +71,13 @@ while [ $COUNTER -lt 150 ]; do
 	
 	# do the merge:
 	$P3S_HOME/tools/accumulator.exe add $MERGE_FILE $f
+	if [ $? -ne 0 ]; then
+	    $P3S_HOME/clients/service.py -n merge_add -m "problem merging $f"
+	    mv $f P3S_PED_DIR
+	fi
 
+	mv $f P3S_PED_DIR
+	
 	# check if merged enough files
 	let COUNTER+=1
 	if [ $COUNTER -ge $MERGE_FACTOR ]; then
@@ -67,25 +89,25 @@ while [ $COUNTER -lt 150 ]; do
     if [ $COUNTER -ge $MERGE_FACTOR ]; then
 	break
     fi
+    # if we came here, this means more statistics are needed. Wait and re-enter the loop
+    sleep $SLEEP
 done
 
-echo number of files merged - $COUNTER
+echo $COUNTER files merged
 
-# create a timestamp-based filename for the merge file
+# create a timestamp-based filename for the merge file, move the merge to the final location
 ts=`date -d "today" +"%Y%m%d%H%M"`
 mergefile=merge$ts.root
-# move the merge to the final location
 mv $MERGE_FILE ../merge/$mergefile
 
 # register the merged file with p3s using the appropriate client:
 argument='{"name":"'$mergefile'","state":"defined","comment":"merged_histograms","datatype":"MERGE","dirpath":"'$P3S_MERGE_DIR'"}'
 $P3S_HOME/clients/dataset.py -r -j $argument
 
-$P3S_HOME/clients/service.py -n acc_add -m "Merge file created: $mergefile in $P3S_MERGE_DIR"
+$P3S_HOME/clients/service.py -n merge_add -m "Merge file created: $mergefile in $P3S_MERGE_DIR"
 
 
 exit
-
 # ta-dah!
 
 
--- inputs/larsoft/merge/lxdqm_acc_init_1.sh
+++ inputs/larsoft/merge/lxdqm_acc_init_1.sh
@@ -10,7 +10,7 @@ source /afs/cern.ch/user/n/np04dqm/public/vp3s/bin/activate
 export P3S_INPUT_DIR=$P3S_DIRPATH/input
 
 if [ ! -d "$P3S_INPUT_DIR" ]; then
-    $P3S_HOME/clients/service.py -n acc_init -m "Problem with directory $P3S_INPUT_DIR"
+    $P3S_HOME/clients/service.py -n merge_init -m "Problem with directory $P3S_INPUT_DIR"
     exit 1
 fi
 
@@ -31,10 +31,10 @@ fi
 mf=`find . -name "$MERGE_FILE" 2>/dev/null`
 if [ -f "$mf" ]; then
     f=`basename $mf`
-    $P3S_HOME/clients/service.py -n acc_init -m "$f exists"
+    $P3S_HOME/clients/service.py -n merge_init -m "$f exists"
     exit
 fi
 
 $P3S_HOME/tools/accumulator.exe init $MERGE_FILE $NCHAN
-$P3S_HOME/clients/service.py -n acc_init -m "$MERGE_FILE created with $NCHAN channels"
+$P3S_HOME/clients/service.py -n merge_init -m "$MERGE_FILE created with $NCHAN channels"
 
