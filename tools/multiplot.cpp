05c0a4f0e2cefa16233302e5093829ee3faece74
Maxim Potekhin <potekhin@bnl.gov>
2018-03-26 20:53:01 -0400

    just incremental edit, checkpoint for the day

--- tools/multiplot.cpp
+++ tools/multiplot.cpp
@@ -8,11 +8,15 @@ int main(int argc, char** argv) {
   TString inputfile(argv[1]);
   TString outputfile(argv[2]);
   const int Nchannels	= std::atoi(argv[3]); // 15360 in the real TPC
+  const int canvX	= std::atoi(argv[4]);
+  const int canvY	= std::atoi(argv[5]);
+
+  const int canvTot	= canvX*canvY;
   
   TFile* inFile= new TFile(inputfile.Data());
 
   TCanvas* cnv = new TCanvas("cnv","Channel histograms", 3200, 1800);
-  cnv->Divide(10,10);
+  cnv->Divide(canvX,canvY);
     
   for(Int_t nch=0; nch<Nchannels; nch++) {
     TH1F* chanHist = (TH1F*) inFile->Get(TString(Form("c%d", nch)));
