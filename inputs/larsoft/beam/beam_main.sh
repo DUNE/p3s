babaadee22e848e896330543b5a34abd3f2d8429
Maxim Potekhin <potekhin@bnl.gov>
2018-04-12 14:12:35 +0200

    add debugging print

--- inputs/larsoft/beam/beam_main.sh
+++ inputs/larsoft/beam/beam_main.sh
@@ -1,5 +1,11 @@
 source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
+echo Finished DUNE setup
 source /afs/cern.ch/work/t/tingjun/public/larsoft/localProducts_larsoft_v06_73_00_e15_prof/setup
+
+echo Will do mrbslp
 mrbslp
+
+echo dir: $P3S_INPUT_DIR
+echo file: $P3S_INPUT_FILE
 
 lar -c beamana_job.fcl $P3S_INPUT_DIR/$P3S_INPUT_FILE -n 1
