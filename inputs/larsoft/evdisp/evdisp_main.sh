cf58e18acd414452321fc73b4b89f7ff6d9bf2a6
Maxim Potekhin <potekhin@bnl.gov>
2018-04-19 16:43:07 -0400

    correct a typo

--- inputs/larsoft/evdisp/evdisp_main.sh
+++ inputs/larsoft/evdisp/evdisp_main.sh
@@ -14,7 +14,7 @@ if [ -z ${P3S_XRD_URI+x} ];
 then
     echo P3S_XRD_URI undefined, using FUSE
     export INPUT_FILE=$P3S_INPUT_DIR/$P3S_INPUT_FILE
-elif
+else
     time xrdcp --tpc first $P3S_XRD_URI/input/$P3S_INPUT_FILE .
     export INPUT_FILE=./$P3S_INPUT_FILE
 fi
