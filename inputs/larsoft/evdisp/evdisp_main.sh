b6b3efb2721add9939d2c8a92f6c2289f021b82c
Maxim Potekhin <potekhin@bnl.gov>
2018-04-19 17:12:00 -0400

    Added cleanup which is crucial for operating in scratch

--- inputs/larsoft/evdisp/evdisp_main.sh
+++ inputs/larsoft/evdisp/evdisp_main.sh
@@ -15,12 +15,14 @@ then
     echo P3S_XRD_URI undefined, using FUSE
     export INPUT_FILE=$P3S_INPUT_DIR/$P3S_INPUT_FILE
 else
-    time xrdcp --tpc first $P3S_XRD_URI/input/$P3S_INPUT_FILE .
+    echo P3S_XRD_URI defined, using xrdcp
+    time xrdcp --silent --tpc first $P3S_XRD_URI/input/$P3S_INPUT_FILE .
     export INPUT_FILE=./$P3S_INPUT_FILE
 fi
 
 
 lar -c $P3S_FCL_LOCAL $INPUT_FILE -T $P3S_OUTPUT_FILE -n$P3S_NEVENTS
+
 
 dest=$P3S_EVDISP_DIR/$P3S_JOB_UUID
 
@@ -35,5 +37,22 @@ for f in $pngs
 do
     [ -s $f ] && cp $f $dest
 done
+
+if [ ! -z ${P3S_XRD_URI+x} ];
+then
+    echo P3S_XRD_URI defined, clean up all files
+    echo ls before
+    ls
+    echo du before
+    du .
+    echo '-----------------'
+    rm ./*
+    echo ls after
+    ls
+    echo du after
+    du .
+    echo '-----------------'
+fi
+
 
 exit 0
