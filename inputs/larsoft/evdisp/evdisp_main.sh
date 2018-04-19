850ef13430482b55c26baa21c24e52e77f3e6ec7
Maxim Potekhin <potekhin@bnl.gov>
2018-04-19 18:31:34 -0400

    Better logic in the event display wrapper

--- inputs/larsoft/evdisp/evdisp_main.sh
+++ inputs/larsoft/evdisp/evdisp_main.sh
@@ -12,11 +12,11 @@ source ${P3S_LAR_SETUP}
 
 if [ -z ${P3S_XRD_URI+x} ];
 then
-    echo P3S_XRD_URI undefined, using FUSE
-    export INPUT_FILE=$P3S_INPUT_DIR/$P3S_INPUT_FILE
+    echo P3S_XRD_URI undefined, using FUSE to stage the data
+    export INPUT_FILE=$P3S_DATA/$P3S_INPUT_DIR/$P3S_INPUT_FILE
 else
     echo P3S_XRD_URI defined, using xrdcp
-    time xrdcp --silent --tpc first $P3S_XRD_URI/input/$P3S_INPUT_FILE .
+    time xrdcp --silent --tpc first $P3S_XRD_URI/$P3S_DATA/$P3S_INPUT_DIR/$P3S_INPUT_FILE .
     export INPUT_FILE=./$P3S_INPUT_FILE
 fi
 
@@ -24,34 +24,37 @@ fi
 lar -c $P3S_FCL_LOCAL $INPUT_FILE -T $P3S_OUTPUT_FILE -n$P3S_NEVENTS
 
 
-dest=$P3S_EVDISP_DIR/$P3S_JOB_UUID
+export DESTINATION=$P3S_DATA/$P3S_EVDISP_DIR/$P3S_JOB_UUID
 
-echo making $dest
-mkdir $dest
+echo making $DESTINATION
+mkdir $DESTINATION
 
 pngs=`ls *.png`
 
 ls -l *.png
 
-for f in $pngs
-do
-    [ -s $f ] && cp $f $dest
-done
-
-if [ ! -z ${P3S_XRD_URI+x} ];
+if [ -z ${P3S_XRD_URI+x} ];
 then
-    echo P3S_XRD_URI defined, clean up all files
-    cd ..
-    echo ls before
-    ls $P3S_JOB_UUID
-    echo du before
-    du $P3S_JOB_UUID
-    echo '-----------------'
-    rm -fr $P3S_JOB_UUID
-    echo ls after
-    ls $P3S_JOB_UUID
-    echo '-----------------'
+    echo P3S_XRD_URI undefined, using FUSE to stage out the data
+    for f in $pngs
+    do
+	[ -s $f ] && cp $f $DESTINATION
+    done
+else
+    echo P3S_XRD_URI defined, using xrdcp
+    time xrdcp --silent --tpc first $f $P3S_XRD_URI/$DESTINATION
 fi
 
+
+cd ..
+echo ls before
+ls $P3S_JOB_UUID
+echo du before
+du $P3S_JOB_UUID
+echo '-----------------'
+rm -fr $P3S_JOB_UUID
+echo ls after
+ls $P3S_JOB_UUID
+echo '-----------------'
 
 exit 0
