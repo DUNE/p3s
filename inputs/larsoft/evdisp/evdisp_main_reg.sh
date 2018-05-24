597c5381158fe1dbb09eab51a7cf71999e832043
Maxim Potekhin <potekhin@bnl.gov>
2018-05-24 17:18:48 -0400

    Adding an event disp wrapper which will include file registration

---
+++ inputs/larsoft/evdisp/evdisp_main_reg.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+export P3S_HOME=/afs/cern.ch/user/n/np04dqm/public/p3s/p3s
+
+if [ -z ${P3S_LAR_SETUP+x} ];
+then
+    echo P3S_LAR_SETUP undefined, exiting
+    exit
+fi
+
+
+source ${P3S_LAR_SETUP}
+
+
+if [ -z ${P3S_XRD_URI+x} ];
+then
+    echo P3S_XRD_URI undefined, using FUSE to stage the data
+    export INPUT_FILE=$P3S_DATA/$P3S_INPUT_DIR/$P3S_INPUT_FILE
+else
+    echo P3S_XRD_URI defined, using xrdcp
+    time xrdcp --silent --tpc first $P3S_XRD_URI/$P3S_DATA/$P3S_INPUT_DIR/$P3S_INPUT_FILE .
+    export INPUT_FILE=./$P3S_INPUT_FILE
+fi
+
+
+lar -c $P3S_FCL_LOCAL $INPUT_FILE -T $P3S_OUTPUT_FILE -n$P3S_NEVENTS
+
+
+export DESTINATION=$P3S_DATA/$P3S_EVDISP_DIR/$P3S_JOB_UUID
+
+echo making $DESTINATION
+mkdir $DESTINATION
+if [ ! -d "$DESTINATION" ]; then
+    echo Directory $DESTINATION was not created, exiting
+    $P3S_HOME/clients/service.py -n evdisp -m "Failed to create $DESTINATION"
+    exit -1
+fi
+pngs=`ls *.png`
+
+# ls -l *.png
+
+if [ -z ${P3S_XRD_URI+x} ];
+then
+    echo P3S_XRD_URI undefined, using FUSE to stage out the data
+    for f in $pngs
+    do
+	[ -s $f ] && cp $f $DESTINATION
+    done
+else
+    echo P3S_XRD_URI defined, using xrdcp
+    for f in $pngs
+    do
+	[ -s $f ] && time xrdcp --silent --tpc first $f $P3S_XRD_URI/$DESTINATION
+    done
+fi
+
+
+cd ..
+echo ls before
+ls $P3S_JOB_UUID
+echo du before
+du $P3S_JOB_UUID
+echo '-----------------'
+rm -fr $P3S_JOB_UUID
+
+# check basic validity of files w/o going into detail
+cd $DESTINATION
+copies=`find . -name "*.png" -exec ls {} \;`
+# -size 0
+
+for copy in $copies
+do
+    ls -l $copy
+done
+    
+
+exit 0
