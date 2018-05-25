9afbeeb18b2376e055656a06657778d74825c945
Maxim Potekhin <potekhin@bnl.gov>
2018-05-25 08:59:52 -0400

    Added environmet initialization for p3s clients to the wrapper

--- inputs/larsoft/evdisp/evdisp_main_reg.sh
+++ inputs/larsoft/evdisp/evdisp_main_reg.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 export P3S_HOME=/afs/cern.ch/user/n/np04dqm/public/p3s/p3s
+export DQM_HOME=/afs/cern.ch/user/n/np04dqm/public/p3s/dqmconfig
+
+source $P3S_HOME/configuration/lxvm_np04dqm.sh > /dev/null
+source /afs/cern.ch/user/n/np04dqm/public/vp3s/bin/activate
+
 
 if [ -z ${P3S_LAR_SETUP+x} ];
 then
@@ -13,10 +18,10 @@ source ${P3S_LAR_SETUP}
 
 if [ -z ${P3S_XRD_URI+x} ];
 then
-    echo P3S_XRD_URI undefined, using FUSE to stage the data
+    echo P3S_XRD_URI undefined, using FUSE to stage in the data
     export INPUT_FILE=$P3S_DATA/$P3S_INPUT_DIR/$P3S_INPUT_FILE
 else
-    echo P3S_XRD_URI defined, using xrdcp
+    echo P3S_XRD_URI defined, using xrdcp to stage in the data
     time xrdcp --silent --tpc first $P3S_XRD_URI/$P3S_DATA/$P3S_INPUT_DIR/$P3S_INPUT_FILE .
     export INPUT_FILE=./$P3S_INPUT_FILE
 fi
@@ -47,13 +52,16 @@ then
 	[ -s $f ] && cp $f $DESTINATION
     done
 else
-    echo P3S_XRD_URI defined, using xrdcp
+    echo P3S_XRD_URI defined, using xrdcp to stage out the data
     for f in $pngs
     do
 	[ -s $f ] && time xrdcp --silent --tpc first $f $P3S_XRD_URI/$DESTINATION
     done
 fi
 
+$P3S_HOME/clients/evdisp.py -a
+
+echo 'done reg'
 
 cd ..
 echo ls before
@@ -63,8 +71,5 @@ du $P3S_JOB_UUID
 echo '-----------------'
 rm -fr $P3S_JOB_UUID
 
-cd $DESTINATION
-$P3S_HOME/clients/evdisp.py -a
-
-echo 'done reg'
+echo 'done'
 exit 0
