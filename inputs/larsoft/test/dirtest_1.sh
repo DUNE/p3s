e501aa4acbcbaacf7e70e47870b8d2e384da8939
Maxim Potekhin <potekhin@bnl.gov>
2018-05-25 19:48:46 -0400

    modify the basic setup test

--- inputs/larsoft/test/dirtest_1.sh
+++ inputs/larsoft/test/dirtest_1.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 echo Sourcing
-source /afs/cern.ch/user/m/mxp/vp3s/bin/activate
+source /afs/cern.ch/user/n/np04dqm/public/vp3s/bin/activate
 python -V
-source /afs/cern.ch/user/m/mxp/projects/p3s/configuration/p3s_setup.sh
+python3.5 -V
+#source /afs/cern.ch/user/m/mxp/projects/p3s/configuration/p3s_setup.sh
 
 hostname
 date
 env | grep P3S
-ls $P3S_INPUT
-ls $P3S_OUTPUT
+env | grep DQM
+#ls $P3S_INPUT
+#ls $P3S_OUTPUT
 exit
