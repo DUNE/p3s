75d3d31b633a94d5c8e6c0f3590a3c3ded1a738b
Maxim Potekhin <potekhin@bnl.gov>
2018-06-21 09:11:00 -0400

    added multi-file job generation and updated the test script for same

--- inputs/jobs/simplejob1.sh
+++ inputs/jobs/simplejob1.sh
@@ -5,11 +5,13 @@ echo pid, ppid: $$ $PPID
 if [ -z ${P3S_INPUT_FILE+x} ];
 then
     echo No input file specified, entering sleep mode
-    /bin/sleep 10
+    /bin/sleep 1
     exit
 fi
 
 
 echo Using input file $P3S_INPUT_FILE
 
-wc -l $P3S_INPUT_FILE > $P3S_OUTPUT_FILE
+f=`wc -l $P3S_INPUT_FILE`
+d=`date`
+echo $d $f >> $P3S_OUTPUT_FILE
