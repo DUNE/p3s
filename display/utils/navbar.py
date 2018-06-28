8b809a4bb46f8beec674f41b661cc87386ef3820
Maxim Potekhin <potekhin@bnl.gov>
2018-06-28 15:33:19 -0400

    restructured the navbar code a bit

--- display/utils/navbar.py
+++ display/utils/navbar.py
@@ -3,41 +3,49 @@ import	django_tables2 as tables
 from django.utils.safestring		import mark_safe
 
 
-def NavBarData(domain):
-    data = []
-    purityTableLink	= mark_safe('<a href="http://'+domain+'/monitor/puritytable">Purity Table</a>')
-    purityChartLink	= mark_safe('<a href="http://'+domain+'/monitor/puritychart">Purity Charts</a>')
-    evdispRunLink	= mark_safe('<a href="http://'+domain+'/monitor/runeventdisplay">Event Display by Run and Event</a>')
-    evdispCatalogLink	= mark_safe('<a href="http://'+domain+'/monitor/eventdisplay">Event Display Image Catalog</a>')
-    monRunLink		= mark_safe('<a href="http://'+domain+'/monitor/monrun">Monitor</a>')
-    
-    data.append({
-        'col1':purityTableLink,
-        'col2':purityChartLink,
-        'col3':evdispRunLink,
-        'col4':evdispCatalogLink,
-        'col5':monRunLink,
-    })
+class Ncolumns(object):
+    N = 0
+        
+    @classmethod
+    def setN(cls, N):
+        cls.N=N
+    @classmethod
+    def getN(cls):
+        return cls.N
 
-    return data
+def NavBarData(domain=''):
+    links = (
+        '<a href="http://'+domain+'/monitor/puritytable">Purity Table</a>',
+        '<a href="http://'+domain+'/monitor/puritychart">Purity Charts</a>',
+        '<a href="http://'+domain+'/monitor/runeventdisplay">Event Display by Run and Event</a>',
+        '<a href="http://'+domain+'/monitor/eventdisplay">Event Display Image Catalog</a>',
+        '<a href="http://'+domain+'/monitor/monrun">Monitor</a>',
+        )
+
+    if(domain==''): return len(links)
+    
+    allLinks = {}
+    for n in range(len(links)): allLinks['col'+str(n+1)] = mark_safe(links[n])
+    return [allLinks,]
     
 class NavTable(tables.Table):
     def __init__(self, *args, **kwargs):
-#        print(args)
-#        print(kwargs)
+        self.Ncolumns = len(args[0][0].keys())
         super(NavTable, self).__init__(*args,**kwargs)
-    col1	= tables.Column()
-    col2	= tables.Column()
-    col3	= tables.Column()
-    col4	= tables.Column()
-    col5	= tables.Column()
+
+    for i in range(5): locals()['col'+str(i+1)] = tables.Column()
     
     def set_site(self, site=''):
         self.site=site
+
+        
     class Meta:
         attrs	= {'class': 'paleblue'}
 
 
 def TopTable(domain):
+    Ncolumns.setN(NavBarData())
+
     t = NavTable(NavBarData(domain), show_header = False)
+
     return t
