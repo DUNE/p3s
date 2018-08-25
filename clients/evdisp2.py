76bd364108c68e0cedf9963fd8e7652dc8ec4985
Maxim Potekhin <potekhin@bnl.gov>
2018-08-25 16:17:09 +0200

    added david's suggestions for the file type labels, added sorting on the directory listing.

--- clients/evdisp2.py
+++ clients/evdisp2.py
@@ -38,13 +38,14 @@ from clientUtils	import takeJson
 
 ########################################################
 fileTypes = OrderedDict([
-    ('adcraw_tpp',		'Raw ADC'),
+    ('adcraw_tpp',		'Raw ADC - pedestal channel vs. tick'),
     ('chmet_ped_tps',		'ADC Pedestals'),
     ('chmet_pedexc_tps',	'ADC pedestal peak bin excess'),
     ('chmet_pedorf_tps',	'ADC pedestal out-of-range fraction'),
-    ('chmet_pedrms_tps',	'ADC pedestal RMS'),
-    ('detprep-',		'Raw ADC Collection View'),
+    ('chmet_pedrms_tps',	'ADC pedestal sigma'),
+    ('detprep-',		'Raw ADC detector display (Collection View)'),
 ])
+
 # ---
 output		= OrderedDict()
 filesDict	= OrderedDict()
@@ -63,10 +64,10 @@ verb	= args.verbosity
 #########################################################
 if(auto):
     output['Category']	= '2D Event Display'
-
+    L = sorted(os.listdir("."))
     for k in fileTypes: #    print('KEY----------------->', k)
         files = []
-        for f in os.listdir("."):
+        for f in L:
             if (f.startswith(k) and f.endswith(".png")): files.append(f)
         if (len(files)>0): filesDict[fileTypes[k]] = ",".join(files)
         
