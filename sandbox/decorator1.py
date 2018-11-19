599da75834fb022878cd880502b89ff0b14a4d08
buddhasystem <potekhin@bnl.gov>
2018-11-19 16:52:21 -0500

    a more interesting example of the decorator

--- sandbox/decorator1.py
+++ sandbox/decorator1.py
@@ -1,9 +1,12 @@
-def p_decorate(func):
-   def func_wrapper(name):
-       return "<p>{0}</p>".format(func(name))
-   return func_wrapper
+def pdec(dec):
+   def p_decorate(func):
+      def func_wrapper(name):
+         return "{0} {1} {0}".format(dec, func(name))
+      
+      return func_wrapper
+   return p_decorate
 
-@p_decorate
+@pdec("foo")
 def get_text(name):
    return "lorem ipsum, {0} dolor sit amet".format(name)
 
